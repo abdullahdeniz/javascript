ba33a674a0daf587edab4c63c353a3a46fca3e57
gelenek <abdullahdenizfr@gmail.com>
2020-06-15 15:31:18 +0200

    add exercises node_params

---
+++ node_params/index.js
@@ -0,0 +1,11 @@
+function sayHello(name){
+    console.log('Nice to meet you '  + name);
+}
+
+sayHello('to my little friend');
+
+console.log(process.argv);
+
+var firstname = process.argv[2];
+
+sayHello(firstname);
